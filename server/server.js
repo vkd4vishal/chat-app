de54a7cba47cc3dadcdbbcd7dc1ada4534797e55
Vishal Kumar <vishalkd0066@gmail.com>
2019-02-14 16:48:56 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname,'../public');
+
+
+
+var app = express();
+const port =3000;
+
+
+
+ 
+   app.use(express.static(publicPath));
+
+
+ 
+
+
+app.listen(3000,()=>{
+    console.log('Server is up on 3000');
+});
