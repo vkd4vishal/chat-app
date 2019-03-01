ff8afba16c18d1bd45dc7ae4fc12f53ad4f8b12b
Vishal Kumar <vishalkd0066@gmail.com>
2019-03-01 21:02:46 +0530

    geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,7 @@ socket.on('newMessage', function (message) {
   li.text(`${message.from}: ${message.text}`);
 
   jQuery('#messages').append(li);
+ 
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -26,4 +27,31 @@ jQuery('#message-form').on('submit', function (e) {
   }, function () {
 
   });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function(){
+  if(!navigator.geolocation){
+    return alert('Geolocation not supported by your browser.');
+  }
+  navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage',{
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  },function(){
+    alert('Unable to fetch location.');
+  });
+});
+
+socket.on('newLocationMessage', function (message) {
+  
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);  
+
+  jQuery('#messages').append(li);
+ 
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage',()=>{
     it('should generate correct message object',()=>{
@@ -9,5 +9,16 @@ describe('generateMessage',()=>{
         var message = generateMessage(from,text);
         expect(message).toMatchObject({from,text});
         expect(typeof message.createdAt).toBe('number');
+    });
+});
+describe('generateLocationMessage',()=>{
+    it('should generate correct location object',()=>{
+        var from = 'vishal';
+        var latitude =1;
+        var longitude =2;
+        var url = `https://www.google.com/maps/?q=${latitude},${longitude}`;
+        var location = generateLocationMessage(from,latitude,longitude);
+        expect(location).toMatchObject({from,url});
+        expect(typeof location.createdAt).toBe('number');
     });
 });
