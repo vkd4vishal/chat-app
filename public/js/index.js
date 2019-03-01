c18b2eb0a0f596a41eea1567bfd0d28e846113fa
Vishal Kumar <vishalkd0066@gmail.com>
2019-03-01 14:38:30 +0530

    ui send message

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,29 @@
 
-
 var socket = io();
+
 socket.on('connect', function () {
-    console.log('Connected to the server');
+  console.log('Connected to server');
 });
-socket.on('welcome',function(message){
-    console.log(message.from);
-    console.log(message.text);
-});
-socket.on('userjoined',function(message){
-    console.log(message.text);
-});
+
 socket.on('disconnect', function () {
-    console.log('Disconnected from the server');
+  console.log('Disconnected from server');
 });
+
 socket.on('newMessage', function (message) {
-    console.log('From',message.from);
-    console.log('New message:',message.text);
-    console.log(message.createdAt);
+  
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
--- server/server.js
+++ server/server.js
@@ -1,37 +1,40 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
-
+const express = require('express');
 const socketIO = require('socket.io');
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '../public');
-const { generateMessage } = require('./utils/message');
 
+const {generateMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-
-
-
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
-    socket.emit('welcome', generateMessage('Admin', 'WELCOME TO THE PMEC CLUB'));
-    socket.broadcast.emit('userjoined', generateMessage('Admin','New User Joined'));
+  console.log('New user connected');
 
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from,message.text));
-        
-    });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
 server.listen(port, () => {
-    console.log(`Server is up on ${port} `);
-}); 
+  console.log(`Server is up on ${port}`);
+});
