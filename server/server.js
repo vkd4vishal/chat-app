de44f76e058f9800a600581cf3269efe3cad9a38
Vishal Kumar <vishalkd0066@gmail.com>
2019-03-04 16:27:03 +0530

    admin to vkd

--- server/server.js
+++ server/server.js
@@ -15,9 +15,9 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('ADMIN', 'Vishal Kumar welcomes you'));
+  socket.emit('newMessage', generateMessage('VKD', 'Vishal Kumar welcomes you'));
 
-  socket.broadcast.emit('newMessage', generateMessage('VKD', 'New user joined'));
+  socket.broadcast.emit('newMessage', generateMessage('ADMIN', 'New user joined'));
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
